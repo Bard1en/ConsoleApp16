2b67df102f4f4882c83aa6dda0035cf7ace52123
Pingvu <nhlpool27@gmail.com>
2024-02-14 12:57:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace f0rtnite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double markLses = double.Parse(Console.ReadLine());
+            double j = double.Parse(Console.ReadLine());
+            Console.WriteLine(pot(markLses, j));
+        }
+        static double pot(double markLses, double j)
+        {
+            double svar = 1;
+            for(double i = 0; i < j; i++) 
+            {
+                svar = markLses *= svar;
+            }
+            return svar;
+        }
+    }
+}
